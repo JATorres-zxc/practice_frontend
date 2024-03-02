8a735273016f338ba915e6df8c9545a3b720c069
JATorres-zxc <angelotorres200425@gmail.com>
2024-03-02 20:47:27 +0800

    added pictures for skill, created review and contact and footer, finished

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active')
 }
+
+var swiper = new Swiper(".skill-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        540: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        540: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
